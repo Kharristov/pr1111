05f1fb1548113485484ad27319f51a643f6595d1
Kharristov <annawey486@gmail.com>
2024-12-05 15:21:08 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static int CountBits(int num)
+    {
+        string binaryRepresentation = Convert.ToString(num, 2);
+
+        int count = 0;
+        foreach (char bit in binaryRepresentation)
+        {
+            if (bit == '1')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для подсчета значащих битов:");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Количество значащих битов: {CountBits(num)}");
+    }
+}
